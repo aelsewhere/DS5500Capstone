ed38318611cd6e378dcd710c2dcdc22e1140c8b1
aelsewhere <68285208+aelsewhere@users.noreply.github.com>
2023-10-21 13:09:56 -0400

    Update ui.R
    clean dashboard for app

--- FantasyApp/ui.R
+++ FantasyApp/ui.R
@@ -1,93 +1,91 @@
 library(shiny)
 library(ggplot2)
+library(DT)
+library(plotly)
+library(bslib)
+library(gridlayout)
 
-# Define UI for application that draws a histogram
-ui <- shinyUI(fluidPage(
-  # Application title
-  titlePanel("Fantasy for Everyone"),
-  
-  navbarPage(
-    "Learn the Game",
-    # ---------------------------------------------------
-    # Home Page -----------------------------------------
-    tabPanel("Home Page",
-             h2("Home Page"),
-             fluidRow(
-               column(5,
-                      imageOutput("example_roster")),
-               column(
-                 10,
-                 h3("Current Roster"),
-                 helpText(
-                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-                 )
-               )
-               
-             )),
-    
-    
-    #---------------------------------------------------
-    # Stats --------------------------------------------
-    tabPanel("Stats",
-             fluidRow(column(
-               5,
-               fluidRow(column(12,
-                               fluidRow(
-                                 column(
-                                   3,
-                                   selectInput(
-                                     "select1",
-                                     label = h3("Model Selection: "),
-                                     choices = list(
-                                       "Model A" = 1,
-                                       "Model B" = 2,
-                                       "Model C" = 3
-                                     ),
-                                     selected = 1
-                                   )
-                                 ),
-                                 column(
-                                   3,
-                                   align = "left",
-                                   selectInput(
-                                     "select2",
-                                     label = h3("selection: "),
-                                     choices = list(
-                                       "Choice 1" = 1,
-                                       "Choice 2" = 2,
-                                       "Choice 3" = 3
-                                     ),
-                                     selected = 1
-                                   )
-                                 )
-                               )))
-             ))),
-    
-    
-    #---------------------------------------------------
-    # Upcoming -----------------------------------------
-    tabPanel("Upcoming",
-             h2("Future Games")),
-    
-    
-    #----------------------------------------------------
-    # Information----------------------------------------
-    tabPanel("Information",
-             p(
-               "Including an informative tab in some way"
-             ),
-             br(),
-             p(
-               "possibly model brakdowns & variable exploration"
-             )),
-    
-    
-    # ---------------------------------------------------
-    # Data -----------------------------------------------
-    tabPanel(
-      "Data",
-      h2("Data Table"),
-      DT::dataTableOutput("fantasy_points")
-    )
-  )
-))
+ui <- grid_page(
+  layout = c(
+    "header  header header",
+    "sidebar area3  area3 ",
+    "sidebar area3  area3 ",
+    "sidebar area3  area3 "
+  ),
+  row_sizes = c("45px",
+                "1fr",
+                "1fr",
+                "1fr"),
+  col_sizes = c("210px",
+                "1.47fr",
+                "0.53fr"),
+  gap_size = "1rem",
+  grid_card(
+    area = "sidebar",
+    h3("My Roster"),
+    card_body(
+      textInput(
+        inputId = "myRosterQB",
+        label = "Quarterback",
+        value = ""
+      ),
+      textInput(
+        inputId = "myRosterRB1",
+        label = "Running Back",
+        value = ""
+      ),
+      textInput(
+        inputId = "myRosterRB2",
+        label = "Running Back",
+        value = ""
+      ),
+      textInput(
+        inputId = "myRosterWR1",
+        label = "Wide Receiver",
+        value = ""
+      ),
+      textInput(
+        inputId = "myRosterWR2",
+        label = "Wide Receiver",
+        value = ""
+      ),
+      textInput(
+        inputId = "myRosterTE",
+        label = "Tight End",
+        value = ""
+      ),
+      textInput(
+        inputId = "myRosterFlex",
+        label = "Flex",
+        value = ""
+      ),
+      textInput(
+        inputId = "myRosterDST",
+        label = "Defense/Special Team",
+        value = ""
+      ),
+      textInput(
+        inputId = "myRosterK",
+        label = "Kicker",
+        value = ""
+      )
+    ),
+    card_footer()
+  ),
+  grid_card_text(
+    area = "header",
+    content = "Fantasy for Everyone",
+    alignment = "start",
+    is_title = TRUE
+  ),
+  grid_card(area = "area3",
+            card_body(
+              tabsetPanel(
+                nav_panel(title = "Upcoming Games"),
+                nav_panel(title = "Predictions"),
+                nav_panel(title = "Player Statistics"),
+                nav_panel(title = "Learn"),
+                nav_panel(title = "Data")
+              )
+            ))
+)
