75fa1212fbd3cb18967d9b8db59cad50a76f8b6d
aelsewhere <68285208+aelsewhere@users.noreply.github.com>
2023-10-23 20:05:22 -0400

    Update master.R

--- FantasyApp/master.R
+++ FantasyApp/master.R
@@ -1,31 +1,20 @@
 # ------------------------------------------------
 # Load Libraries ---------------------------------
-
 library(shiny)
 library(shinyMobile)
 
-
-
 # Load helper files -----------------------------
-
-
+source("helper.R")
 
 # Create Master Variables ----------------------
 
-
-
 # Load Data -------------------------------------
-
-
-
 
 # Load UI ----------------------------------------
 source("ui.R")
 
-
 # Server Logic -----------------------------------
 source("server.R")
 
-
 # Run app ----------------------------------------
-shinyApp(ui=ui, server=server)
+shinyApp(ui = ui, server = server)
