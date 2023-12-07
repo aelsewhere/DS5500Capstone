0ff8e94e73a004330b8f2c47b4f5bceb387071ab
aelsewhere <68285208+aelsewhere@users.noreply.github.com>
2023-12-07 17:55:50 -0500

    Update master.R
    adding a new run file

--- FantasyApp/master.R
+++ FantasyApp/master.R
@@ -1,17 +1,15 @@
-# ------------------------------------------------
-# Load Libraries ---------------------------------
-library(shiny)
-# library(shinyMobile)
+runApp <- function() {
+  # ------------------------------------------------
+  # Load Libraries ---------------------------------
+  library(shiny)
+  # library(shinyMobile)
 
-# Load helper files -----------------------------
-source("helper.R")
+  # Load helper files -----------------------------
+  source("helper.R")
 
-# Load UI ----------------------------------------
-source("ui.R")
+  # Load UI ----------------------------------------
+  source("ui.R")
 
-# Server Logic -----------------------------------
-source("server.R")
-
-# Run app ----------------------------------------
-shinyApp(ui = ui, server = server)
-
+  # Server Logic -----------------------------------
+  source("server.R")
+}
