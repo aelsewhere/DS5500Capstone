779372d9a70c6d7a96532f6e0706fd7ea420cb9a
aelsewhere <68285208+aelsewhere@users.noreply.github.com>
2023-12-08 20:10:55 -0500

    Update master.R

--- FantasyApp/master.R
+++ FantasyApp/master.R
@@ -1,15 +1,16 @@
-runApp <- function() {
-  # ------------------------------------------------
-  # Load Libraries ---------------------------------
-  library(shiny)
-  # library(shinyMobile)
+# ------------------------------------------------
+# Load Libraries ---------------------------------
+library(shiny)
+# library(shinyMobile)
 
-  # Load helper files -----------------------------
-  source("helper.R")
+# Load helper files -----------------------------
+source("helper.R")
 
-  # Load UI ----------------------------------------
-  source("ui.R")
+# Load UI ----------------------------------------
+source("ui.R")
 
-  # Server Logic -----------------------------------
-  source("server.R")
-}
+# Server Logic -----------------------------------
+source("server.R")
+
+# Run Shiny App
+shinyApp(ui = ui, server = server)
